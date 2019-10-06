4bca48fbb7d0e583e1e9ea1b0ae1f4f76ce8e5f6
Paul Gallagher <paulitogalalgher@gmail.com>
2019-10-06 13:39:00 -0400

    Begun writing tests...

--- apicbase/tests.py
+++ apicbase/tests.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 from .models import Recipe, Ingredient
 
 class RecipeModelTests(TestCase):
-    
+    def ensure_recipe_has_ingredients_question(self):
+        test_recipe = Recipe(recipe_name="Recipe", recipe_desc="Description")
     pass
 class IngredientModelTests(TestCase):
     pass
